bac243049fe5467b2baa00128d0c825a53d3aeb0
Erwan Setyo Budi <erwans818@gmail.com>
2024-12-03 19:01:59 +0700

    Update fines_report.php

--- fines_report.php
+++ fines_report.php
@@ -16,10 +16,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * 
  */
 
-/* Fines Report */
+/* Fines Report by Erwan Setyo Budi*/
 
 // key to authenticate
 define('INDEX_AUTH', '1');
